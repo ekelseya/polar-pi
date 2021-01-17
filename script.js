335fa2bf29a0de40365ddb8b5b07c8600fa05a16
ekelseya <42783392+ekelseya@users.noreply.github.com>
2021-01-16 17:28:37 -0700

    Add comments and clean code

--- script.js
+++ script.js
@@ -18,10 +18,13 @@ const ctx = canvas.getContext('2d');
 // ctx.translate(500, 500); //Arc path
 ctx.translate(200, 100);
 
+// mark origin for reference
 ctx.fillStyle = 'red';
 ctx.fillRect(0, 0, 5, 5);
 
+// convert degrees into radians
 const radCalc = degrees => (degrees)*Math.PI/180;
+// convert polar coordinates to cartesian coordinates
 const xConvert = (r, theta) => (r * Math.cos(theta));
 const yConvert = (r, theta) => (r * Math.sin(theta));
 
@@ -39,12 +42,9 @@ function render() {
     let theta = radCalc(r*36);
     let x = -(xConvert(r, theta)) * 10;
     let y = -(yConvert(r, theta)) * 10;
-    // ctx.arcTo(0, 0, x, y, r*18);
+    // ctx.arcTo(0, 0, x, y, r*18); // Arc path
     ctx.lineTo(x, y);
     ctx.stroke();
     ctx.translate(x, y);
-
-    if (count < 1002) {
-        count++;
-    }
+    count++;
 }
